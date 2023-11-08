ca5202d954404583cb91bc9520129a905fd45e36
Vincent-Tann <1506186970@qq.com>
2023-11-08 13:32:52 +0800

    some updates

--- config.py
+++ config.py
@@ -336,4 +336,28 @@ confs_scanrefer={
         'use_code_interpreter':True,
         'tool':'3dvista_200c'
     },
+    7:{
+        'dataset':'scanrefer',
+        'model':'gpt-4', # 4 with 3dvista without filtering pc
+        # 'model':'gpt-4-1106-preview', # 4 with 3dvista without filtering pc
+        'result_folder_name':'eval_results_scanrefer_4_p_3dvista_valset/',
+        'use_principle':True,
+        'refer_dataset_path':"./data/scanrefer/ScanRefer_filtered_val_sampled1000.json",
+        'use_gt_box':False,
+        'use_priority':False,
+        'use_code_interpreter':True,
+        'tool':'3dvista_200c_wof'
+    },
+    8:{
+        'dataset':'scanrefer',
+        # 'model':'gpt-4', # 4 with 3dvista without filtering pc
+        'model':'gpt-4-1106-preview', # 4-turbo with 3dvista without filtering pc
+        'result_folder_name':'eval_results_scanrefer_4tb_p_3dvista_valset/',
+        'use_principle':True,
+        'refer_dataset_path':"./data/scanrefer/ScanRefer_filtered_val_sampled1000.json",
+        'use_gt_box':False,
+        'use_priority':False,
+        'use_code_interpreter':True,
+        'tool':'3dvista_200c_wof'
+    },
 }
--- object_filter_gpt4.py
+++ object_filter_gpt4.py
@@ -29,6 +29,7 @@ class ObjectFilter(Dialogue):
     def __init__(self):
         config = {
         'model': 'gpt-4',
+        # 'model': 'gpt-4-1106-preview',
         'temperature': 0,
         'top_p': 0.0,
         'max_tokens': 'inf',
